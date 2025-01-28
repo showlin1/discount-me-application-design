4e07ec17edfd992c844c356038b7a79afcb1d82f
Showlin Akter <showlinakter77@gmail.com>
2025-01-28 18:13:39 +0600

    create a navbar section

--- src/Component/Layout/Main.jsx
+++ src/Component/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 
 const Main = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
